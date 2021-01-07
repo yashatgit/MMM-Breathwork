d36d9883365a3672ea5ed180287ab09a4d0c221a
yashSoni <ysoni@sprinklr.com>
2021-01-07 08:18:35 +0530

    CSS only animations

--- MMM-Breathwork.js
+++ MMM-Breathwork.js
@@ -1,18 +1,7 @@
-const breathPatterns = {
-  magic: [
-    { millis: 5.5 * 1000, text: "inhale", mode: "inhale" },
-    { millis: 5.5 * 1000, text: "exhale", mode: "exhale" },
-  ],
-};
-
 Module.register("MMM-Breathwork", {
   start: function () {
     const self = this;
     Log.info("Now Starting module: " + self.name);
-
-    setTimeout(() => {
-      self.initModule();
-    }, 2000);
   },
 
   getStyles: function () {
@@ -23,39 +12,11 @@ Module.register("MMM-Breathwork", {
     const wrapper = document.createElement("div");
     wrapper.innerHTML = `
       <div id="MMM-Breathwork-outer-cir">
-        <div id="MMM-Breathwork-inner-cir">    
-        </div>
-        <div id="MMM-Breathwork-cir-txt">breathe</div>
+        <div id="MMM-Breathwork-inner-cir"></div>
+        <div class="MMM-Breathwork-text-breathe MMM-Breathwork-text-breathe-in">Inhale</div>
+        <div class="MMM-Breathwork-text-breathe MMM-Breathwork-text-breathe-out">Exhale</div>  
       </div>
       `;
     return wrapper;
-  },
-
-  initModule: function () {
-    const innerCircle = document.getElementById("MMM-Breathwork-inner-cir");
-    const circleText = document.getElementById("MMM-Breathwork-cir-txt");
-
-    const startBreath = (breathConfig) => {
-      const totalBreathSpan = breathConfig.reduce(
-        (acc, i) => acc + i.millis,
-        0
-      );
-      const updateNode = (config) => {
-        circleText.innerHTML = config.text;
-        innerCircle.setAttribute("data-mode", config.mode);
-        console.log(+new Date());
-      };
-
-      setInterval(() => {
-        updateNode(breathConfig[0]);
-      }, totalBreathSpan);
-
-      setTimeout(() => {
-        setInterval(() => {
-          updateNode(breathConfig[1]);
-        }, totalBreathSpan);
-      }, breathConfig[0].millis);
-    };
-    startBreath(breathPatterns.magic);
   },
 });
